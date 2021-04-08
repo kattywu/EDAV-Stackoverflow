59561af9ad83ac8aa97acedbe4fe780d10caf6c4
Yingyao Wu <62625497+kattywu@users.noreply.github.com>
2021-04-08 17:07:58 -0400

    edited cleveland dot plot; started intro

--- docs/_include/js/bigdata.parallelcoordinates.js
+++ docs/_include/js/bigdata.parallelcoordinates.js
@@ -1,20 +1,22 @@
-   var graph;
-    var dataset;
+var graph;
+var dataset;
     
-    d3.csv('_include/data/df.csv', function(data) {
+    d3.csv("https://raw.githubusercontent.com/kattywu/EDAV-Stackoverflow/main/_include/data/df.csv?token=ASRDPN3XKH3QNQEFSV67JK3AORLTI", function(data) {
     dataset = data;
       graph = d3.parcoords()('#wrapper')
                 .data(data)
-                .alpha(0.4)
+                .alpha(0.3)
+                .margin({ top: 24, left: 75, bottom: 12, right: 0 })
                 .mode("queue")
-                .rate(5)
+                .rate(20)
                 .render()
                 .interactive()
                 .brushable()
+                .reorderable()
                 
-
-
-     change_color("TagRank");
+                
+    //Set starting color to year
+     change_color("Year");
 
       graph.svg
            .selectAll(".dimension")
@@ -31,7 +33,7 @@
             "mouseout": graph.unhighlight
           });
 
-      graph.on("brush", function(d) {
+      graph.on("brush", function(d){
         d3.select("#grid")
           .datum(d.slice(0,10))
           .call(grid)
@@ -43,65 +45,71 @@
       });
   });
   
-       // Remove all but selected from the dataset
+       // Remove everything not selected
       d3.select("#keep-data")
         .on("click", function() {
           new_data = graph.brushed();
           if (new_data.length == 0) {
-            alert("Please do not select all the data when keeping/excluding");
+            alert("Please do not remove all the data");
             return false;
           }
-          callUpdate(new_data);
+          UpdateGraph(new_data);
         });
     
-      // Exclude selected from the dataset
-      d3.select("#exclude-data")
+      // Remove selected from the dataset
+      d3.select("#remove-data")
         .on("click", function() {
           new_data = _.difference(dataset, graph.brushed());
           if (new_data.length == 0) {
-            alert("Please do not select all the data when keeping/excluding");
+            alert("Please do not remove all the data");
             return false;
           }
-          callUpdate(new_data);
+          UpdateGraph(new_data);
         });
         
-      
+      // Start over
       d3.select("#reset-data")
          .on("click", function() {
-               callUpdate(dataset);
+               UpdateGraph(dataset);
          });
+         
       
 
     var color_scale = d3.scale.linear()
-                        .domain([-2,-0.5,0.5,2])
-                        .range(["#DE5E60", "steelblue", "steelblue", "#98df8a"])
+                        .range(['#ddaa33', '#bb5566', '#004488'])
                         .interpolate(d3.interpolateLab);
 
-    function change_color(dimension) {
-      graph.svg.selectAll(".dimension")
-        .style("font-weight", "normal")
-        .filter(function(d) { return d == dimension; })
-        .style("font-weight", "bold")
 
-      graph.color(zcolor(graph.data(),dimension)).render()
-    }
-
-    function zcolor(col, dimension) {
-      var z = zscore(_(col).pluck(dimension).map(parseFloat));
-      return function(d) { return color_scale(z(d[dimension]))}
-    };
-
-    function zscore(col) {
-      var n = col.length,
-      mean = _(col).mean(),
-      sigma = _(col).stdDeviation();
-
-      return function(d) {
-        return (d-mean)/sigma;
-      };
-    };
     
-    function callUpdate(data) {
+    	function change_color(dimension) {
+		// change the fonts to bold
+		graph.svg.selectAll(".dimension")
+				.style("font-weight", "normal")
+				.filter(function(d) { return d == dimension; })
+				.style("font-weight", "bold");
+		//add ordinal scale for categorical variable
+    if(dimension=="Tags"){
+      color_scale = d3.scale.ordinal()
+      .range(["#a6cee3","#1f78b4","#b2df8a","#33a02c",
+            "#fb9a99","#e31a1c","#fdbf6f","#ff7f00",
+            "#cab2d6","#6a3d9a","#ffff99","#b15928"]);
+    }else{
+      color_scale = d3.scale.linear()
+                        .range(['#ddaa33', '#bb5566', '#004488'])
+                        .interpolate(d3.interpolateLab);
+    }
+		// set domain of color scale
+		var values = graph.data().map(function(d){return parseFloat(d[dimension])});
+		color_scale.domain([d3.min(values),d3.mean(values), d3.max(values)]);
+
+		// change colors for each line
+		graph.color(function(d){return color_scale([d[dimension]])}).render();
+	};
+
+    
+    function UpdateGraph(data) {
              graph.data(data).brush().render().updateAxes();
              
     }
+    
+      
